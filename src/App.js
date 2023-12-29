eeba88f227392cf9be9852b4d8d2a2c331f37ad8
Lucas Cervelli Haderne <lucasch98.lch@gmail.com>
2023-12-29 05:25:43 -0300

    Try add boostrap to project
    Not work styles yet

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import TableTasks from './components/TableTasks.js';
+
 function App() {
   return (
     <div className="App">
--- src/components/ListTasks.js
+++ src/components/ListTasks.js
@@ -1,29 +1,29 @@
 import "../styles/ListTasks.css";
 import { MdDelete } from "react-icons/md";
+import { Table }  from 'react-bootstrap';
 
 const ListTasks = ({tasks, deleteTask}) =>{
   return(
-    <div className="listTasks">
-      <table>
-        <thead>
-          <tr>
-            <th>Task</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          {tasks && tasks.map((task, index) => ( //Chequeo que el array esta inicializado
-            <tr key={index}>
-              <td>{task}</td>
-              <tr>
-                <MdDelete className="deleteIcon" onClick={() => deleteTask(index)}/>
-              </tr>
-
+    <Table striped bordered hover className="listTasks">
+        <table>
+          <thead>
+            <tr>
+              <th>Task</th>
+              <th>Action</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+          </thead>
+          <tbody>
+            {tasks && tasks.map((task, index) => ( //Chequeo que el array esta inicializado
+              <tr key={index}>
+                <td>{task}</td>
+                <tr>
+                  <MdDelete className="deleteIcon" onClick={() => deleteTask(index)}/>
+                </tr>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+    </Table>
     );
 }
 
